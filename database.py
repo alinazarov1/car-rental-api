9248adc8a0ad8c7419c231636f7f04ab169cc845
alinazarov1 <alinazarovrozimuhammad9@gmail.com>
2026-03-08 13:16:19 +0100

    Switch to PostgreSQL database

--- database.py
+++ database.py
@@ -1,11 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./cars.db"
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL", 
+    "postgresql://car_rental_db_6sv4_user:ESli87WO1FArjhxXicBerwT2ZGmBpdfr@dpg-d6mmc5fafjfc7397ensg-a.oregon-postgres.render.com/car_rental_db_6sv4"
+)
+
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False}
+    SQLALCHEMY_DATABASE_URL
 
 )
 
